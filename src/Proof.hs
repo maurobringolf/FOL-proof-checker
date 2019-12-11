25fcc64babfbad1599da5c6353338bf07f3f237b
Mauro Bringolf <maurob@student.ethz.ch>
2019-12-11 18:12:58 +0100

    Add axiom schemata checking functions

--- src/Proof.hs
+++ src/Proof.hs
@@ -6,8 +6,84 @@ type Proof = [Formula]
 
 type Context = [Formula]
 
-logicalAxioms :: Context
-logicalAxioms = []
+l1 f = case f of
+  Imp f1 (Imp f2 f3) -> f1 == f3 
+  _ -> False
+
+-- TODO
+l2 f = case f of
+  _ -> False
+
+l3 f = case f of
+  Imp (And f1 f2) f3 -> f1 == f3 
+  _ -> False
+
+l4 f = case f of
+  Imp (And f1 f2) f3 -> f2 == f3 
+  _ -> False
+
+l5 f = case f of
+  Imp f1 (Imp f2 (And f3 f4)) -> f1 == f3 && f4 == f2
+  _ -> False
+
+-- TODO
+l6 f = case f of
+  _ -> False
+  
+-- TODO
+l7 f = case f of
+  _ -> False
+
+-- TODO
+l8 f = case f of
+  _ -> False
+
+-- TODO
+l9 f = case f of
+  _ -> False
+
+-- TODO
+l10 f = case f of
+  _ -> False
+
+-- TODO
+l11 f = case f of
+  _ -> False
+
+-- TODO
+l12 f = case f of
+  _ -> False
+
+-- TODO
+l13 f = case f of
+  _ -> False
+
+-- TODO
+l14 f = case f of
+  Eq t1 t2 -> t1 == t2
+  _ -> False
+
+-- TODO
+l15 f = case f of
+  _ -> False
+
+logicalAxiom :: Formula -> Bool
+logicalAxiom f = foldr (\l b -> b || l f) False [ l1
+                                                , l2
+                                                , l3
+                                                , l4
+                                                , l5
+                                                , l6
+                                                , l7
+                                                , l8
+                                                , l9
+                                                , l10
+                                                , l11
+                                                , l12
+                                                , l13
+                                                , l14
+                                                , l15
+                                                ]
 
 data Result = Correct
             | Incorrect Context Formula
@@ -27,7 +103,7 @@ checkProof axioms proof = if null proof then Correct else
   let phi = head proof
       phi_before = tail proof
   in
-    if (phi `elem` logicalAxioms ||
+    if (logicalAxiom phi ||
     phi `elem` axioms ||
     any (\(phi1, phi2) -> phi1 == Imp phi2 phi) [(phi1, phi2) | phi1 <- phi_before, phi2 <- phi_before])
     then
