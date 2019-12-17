4dfad0909aa15c2a7fffd1b107a04270d3b8522b
Mauro Bringolf <maurob@student.ethz.ch>
2019-12-17 14:23:32 +0100

    Implement a simplistic CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
-module Main where
+import System.Environment
+import System.Exit
+
+import Parser
+import Proof
 
 main :: IO ()
-main = do putStrLn "No CLI interface implemented."
+main = getArgs >>= run
+
+run []     = usage   >> exitWith ExitSuccess
+run ["-h"] = usage   >> exitWith ExitSuccess
+run ["-v"] = version >> exitWith ExitSuccess
+run [file] = do text <- readFile file
+                let (sig, ctxt, proof) = parse text
+                let result             = checkProof ctxt proof
+                putStrLn $ show $ result
+                if result == Correct then
+                  exitWith ExitSuccess
+                else
+                  exitWith (ExitFailure 1)
+run _      = usage   >> exitWith ExitSuccess
+
+usage   = putStrLn "Usage: fol-proof-checker [-vh] [file]"
+version = putStrLn "fol-proof-checker 1.0.0"
