1bd10ea7dcff6167597e3dab39a198fd61832a1f
Mauro Bringolf <maurob@student.ethz.ch>
2020-02-16 10:43:16 +0100

    Name group axioms

--- src/Theory/GT.hs
+++ src/Theory/GT.hs
@@ -6,9 +6,9 @@ import Context
 
 axioms :: Context
 axioms = [ -- GT_0 TODO
-           -- GT_1
+           -- GT_1 (Left-neutral element): ∀x e ∘ x = x
            Literal $ FA "x" (Rel "=" [FApp "∘" [Const "e", Var "x"], Var "x"])
-           -- GT_2 TODO
+           -- GT_2 (Left-inverse element): ∀x∃y y ∘ x = e
          , Literal $ FA "x" $ EX "y" $ Rel "=" [FApp "∘" [Var "y", Var "x"], Const "e"]
          ]
 
